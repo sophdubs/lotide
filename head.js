0d5edf033d9ed4f5b2bfb5f80d1033be66c7fbe6
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-05-25 15:25:36 -0400

    Implemented head function which returns the first element in an array. If the array is empty, it returns undefined. Used the asserEqual function (implemented earlier) to test my code, all tests are passing

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(['only']), 'only');
+assertEqual(head([]), undefined);
