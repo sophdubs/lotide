b4fbe4f00ef1c52c307918529d5e6c7dada10546
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-05-26 14:16:13 -0400

    Successfully comparing two arrays returning true if they are the same and false otherwise

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), true); // => should FAIL
