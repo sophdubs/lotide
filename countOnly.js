52cf9cc74d6a5a978a464765b554b84352af0219
Sophie <sophiemargueritedubois@gmail.com>
2020-05-27 18:20:48 +0000

    Followed the tutorial on lighthouse and completed the function countOnly. Passing all assertions.

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+// Test cases
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
