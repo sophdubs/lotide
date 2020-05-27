e9b11918a404f1b530f7c6b096cf3c8a85bc1213
Sophie <sophiemargueritedubois@gmail.com>
2020-05-27 17:22:10 +0000

    Created middle document and copied assertArraysEqual and eqArrays

---
+++ middle.js
@@ -0,0 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  if (result) {
+    console.log(`🍏  Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
