bc14221999b4e27fd3a1f53640e4e88aa4df294c
Sophie <sophiemargueritedubois@gmail.com>
2020-05-27 19:23:44 +0000

    Implemented eqObject in such a way that is passes if all values are primitive values

---
+++ eqObjects.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) return false;
+
+  for ([key, value] of Object.entries(object1)) {
+    if (!object2[key] || object2[key] !== value) return false;
+  }
+  return true;
+}
+
+// Primitives as values tests:
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, ba), true); 
+assertEqual(eqObjects(ab, abc), false); 
