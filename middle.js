c1a576add2c7d98c57294c966c3aa5d553f9fd7e
Sophie <sophiemargueritedubois@gmail.com>
2020-05-27 17:43:22 +0000

    Completed implementation of middle function. Also added test cases and all tests are passing

--- middle.js
+++ middle.js
@@ -1,3 +1,4 @@
+// Previously implemented
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
 
@@ -8,6 +9,7 @@ const eqArrays = function(arr1, arr2) {
   return true;
 }
 
+// Previously implemented
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2);
   if (result) {
@@ -17,10 +19,38 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 }
 
+// Helper function to determine if num is even. Returns boolean. 
 const isEven = function(num) {
   return num % 2 === 0;
 }
 
+// Helper function to calculated index of middle value in the array. 
+const calculateMiddleIndex = function(arr) {
+  return Math.floor(arr.length / 2);
+}
+
 const middle = function(inputArr) {
+  // Handles cases where input array is length 0, 1, or 2. Returns empty array. 
+  if (inputArr.length <= 2) return [];
+  
+  // Call helper function to determine middle index
+  let middleIndex = calculateMiddleIndex(inputArr);
+  
+  if (isEven(inputArr.length)) {
+    // If array.length is even, we return the values at middleIndex -1 and middle index inside a new array.
+    return [inputArr[middleIndex - 1], inputArr[middleIndex]];
+  } else {
+    // Else, we return the value at middleIndex in a new array. 
+    return [inputArr[middleIndex]];
+  }
 
 }
+
+// Passing all tests
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); 
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
