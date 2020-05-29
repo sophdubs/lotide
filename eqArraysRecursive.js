d907bc28ffc7e8183162638cbdf3079112df14ca
Sophie <sophiemargueritedubois@gmail.com>
2020-05-29 20:57:48 +0000

    Implemented eqArrays recursively to handle nested arrays

---
+++ eqArraysRecursive.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i])) {
+      if (!Array.isArray(arr2[i])) {
+        return false;
+      } else {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      }
+    } else {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
+
+
+// Simple Arrays
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+
+// Nested Arrays
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+
