641f46f210984844999272f0627eb19e55144c56
Sophie <sophiemargueritedubois@gmail.com>
2020-06-01 18:40:50 +0000

    Removed unecessary comments

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,3 @@
-
-
-// // Basic tests
-// assertEqual(tail([1, 2, 3]).length, 2);
-// assertEqual(tail([1, 2, 3])[0], 2);
-// assertEqual(tail([1, 2, 3])[1], 3);
-
-// // Making sure original array is not mutated by tail function
-// let testArray = ['cat', 'dog', 'bird', 'rabbit'];
-// let tailedArray = tail(testArray);
-// assertEqual(testArray.length, 4);
-// assertEqual(tailedArray.length, 3);
-
-// // Array length 1 should return empty array
-// assertEqual(tail(['single']).length, 0);
-// assertEqual(Array.isArray(tail(['single'])), true);
-
-// // Empty array should return empty array
-// assertEqual(tail([]).length, 0);
-// assertEqual(Array.isArray(tail([])), true);
 const expect = require('chai').expect;
 const tail   = require('../tail');
 
