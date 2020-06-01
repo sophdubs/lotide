29a7222cd524f16e4117a48e7d72f477769e9caf
Sophie <sophiemargueritedubois@gmail.com>
2020-06-01 18:47:21 +0000

    Refactored middleTest to use Mocha/Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const expect = require('chai').expect;
+const middle   = require('../middle');
 
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+describe("#middle", () => {
+  it("returns [] for input []", () => {
+    expect(middle([])).to.deep.equal([]);
+  });
+  it("returns [] for input [1]", () => {
+    expect(middle([1])).to.deep.equal([]);
+  });
+  it("returns [] for input [1, 2]", () => {
+    expect(middle([1, 2])).to.deep.equal([]);
+  });
+  it("returns [2] for input [1, 2, 3]", () => {
+    expect(middle([1, 2, 3])).to.deep.equal([2]);
+  });
+  it("returns [2, 3] for input [1, 2, 3, 4]", () => {
+    expect(middle([1, 2, 3, 4])).to.deep.equal([2, 3]);
+  });
+});
