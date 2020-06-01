9bcf78ff1e3dd8a94295a191752b2fb5d11bb8fc
Sophie <sophiemargueritedubois@gmail.com>
2020-06-01 17:45:09 +0000

    Refactored head.js (removed asserEqual function, moved test cases to its own file, and export head.js)

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🍏  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🍎  Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(['only']), 'only');
-assertEqual(head([]), undefined);
+module.exports = head;
