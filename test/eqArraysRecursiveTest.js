af6c290b6124f7d5903ae862563409bac3341271
Sophie <sophiemargueritedubois@gmail.com>
2020-06-01 20:54:52 +0000

    cleaned up undecessary comments

--- test/eqArraysRecursiveTest.js
+++ test/eqArraysRecursiveTest.js
@@ -15,13 +15,3 @@ describe("#eqArraysRecursive", () => {
     assert.strictEqual(eqArraysRecursive([[2, 3], [4]], [[2, 3], [4, 5]]), false);
   });
 });
-
-// Simple Arrays
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-
-// // Nested Arrays
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
