1f8eeec08b9f726a929451a7f1ff6419ddf39d78
Sophie <sophiemargueritedubois@gmail.com>
2020-06-01 21:23:49 +0000

    Refactored flatten.js and created a test file for it

--- flatten.js
+++ flatten.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
-}
-
-const assertArraysEqual = function(arr1, arr2) {
-  let result = eqArrays(arr1, arr2);
-  if (result) {
-    console.log(`🍏  Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🍎  Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
-
 const flatten = function(arr) {
   let solutionArray = [];
   for (let i = 0; i < arr.length; i++) {
@@ -29,7 +10,4 @@ const flatten = function(arr) {
   return solutionArray;
 }
 
-assertArraysEqual(flatten([1, [2, 3]]), [1, 2, 3]); // <= SHOULD PASS
-assertArraysEqual(flatten([[]]), []); // <= SHOULD PASS
-assertArraysEqual(flatten([1, [2, 3, 4], 5, [6, 7, 8]]), [1, 2, 3, 4, 5, 6, 7, 8]); // <= SHOULD PASS
-assertArraysEqual(flatten([1, [2, 3, 4], 5, [6, 7, 8]]), [1, 2]); // <= SHOULD FAIL
+module.exports = flatten;
---
+++ test/flattenTest.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+const flatten= require('../flatten');
+
+describe("#flatten", () => {
+  it("returns a flattened single-level array when passed in a nested array", () => {
+    expect(flatten([1, [2, 3]])).to.deep.equal([1, 2, 3]);
+  });
+  it("returns an empty flattened single-level array when passed in a nested empty array", () => {
+    expect(flatten([[]])).to.deep.equal([]);
+  });
+});
